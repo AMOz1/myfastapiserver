dcb0fb31d7c454199d0b05332ce0c7cf39ca6122
O2Z <ozrenn@gmail.com>
2024-01-06 13:19:17 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,13 @@
+from main import app
+from uvicorn import Config, Server
+
+class WSGIAdapter:
+    def __init__(self, asgi_app):
+        self.app = asgi_app
+
+    def __call__(self, environ, start_response):
+        server = Server(Config(self.app, loop="none", http="none"))
+        server.run()
+
+wsgi_app = WSGIAdapter(app)
 
