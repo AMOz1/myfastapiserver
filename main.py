8f81bb5dfea4cb7dc0ba4d2664d75df05930fa27
O2Z <ozrenn@gmail.com>
2024-01-08 08:25:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-from txtai.embeddings import Embeddings
 
 app = FastAPI()
 
-# Initialize txtai embeddings instance
-embeddings = Embeddings("app.yml")
 
-@app.get("/search")
-def search(query: str):
-    return embeddings.search(query, 1)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
